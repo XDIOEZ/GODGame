de97939cae0755647d0b3801d4a9ebad8dc9db55
yelanfengsheng <2143309490@qq.com>
2025-08-22 17:52:55 +0800

    风力发射器  开始场景

---
+++ Assets/Scripts/UI/StartScene/StartController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class StartController : MonoBehaviour
+{
+    public Button continueGameBtn;
+    public Button newGameBtn;
+    public Button quitBtn;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        continueGameBtn.onClick.AddListener(OnContinueGame);
+        newGameBtn.onClick.AddListener(OnNewGame);
+        quitBtn.onClick.AddListener(OnQuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnContinueGame()
+    {
+        Debug.Log("Continue Game button clicked");
+        // Add logic to continue the game
+    }
+    public void OnNewGame()
+    {
+        Debug.Log("New Game button clicked");
+        // Add logic to start a new game
+        //跳转到游戏场景
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+    public void OnQuitGame()
+    {
+        Debug.Log("Quit Game button clicked");
+        // Add logic to quit the game
+        Application.Quit();
+    }
+}
